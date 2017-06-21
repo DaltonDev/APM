5692fd90a0cbe4a802177154e8dcd45b2a7da8ff
DaltonDev <lundgren.dev@gmail.com>
2017-06-21 18:00:09 +0200

    module 8.4

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component } from '@angular/core'
 @Component({
-    selector: 'pm-app',
-    template: `
-    <div><h1>{{pageTitle}}</h1>
-        <div>My first Component</div>
-    `
+  selector: 'pm-app',
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+  <pm-products></pm-products>
+  </div>
+  `
 })
-export class AppComponent {
+export class AppComponent{
   pageTitle: string = 'Acme Product Management';
-  }
+}
